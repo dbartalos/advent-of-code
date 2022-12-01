c74db0ace20aee5198ad00f6a932f04b58bd4c77
dbartalos <6267612+dbartalos@users.noreply.github.com>
2022-12-01 11:53:57 +0000

    part2

--- 2022/days/01.py
+++ 2022/days/01.py
@@ -4,6 +4,7 @@ import sys
 
 current = 0
 most = 0
+totals = []
 
 with open(sys.argv[1], 'r') as input_data:
   for line in input_data:
@@ -13,4 +14,5 @@ with open(sys.argv[1], 'r') as input_data:
       if current > most:
         most = current
       current = 0
-  print(most)
+    totals.append(current)
+  print(sum(sorted(totals, reverse=True)[:3]))
